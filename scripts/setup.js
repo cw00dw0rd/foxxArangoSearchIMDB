534599f33285652b94fbeb4b70125a602f46ca2c
Chris Woodward <cw00dw0rd@gmail.com>
2019-10-29 11:04:13 -0400

    added view setup

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,7 +1,50 @@
 'use strict';
 const db = require('@arangodb').db;
 const collectionName = 'imdb_vertices';
+const viewName = 'imdbView';
 
 if (!db._collection(collectionName)) {
   db._createDocumentCollection(collectionName);
+}
+
+if (!db._view(viewName)) {
+  db._createView(viewName, 'arangosearch', {
+  "links": {
+    "imdb_vertices": {
+      "analyzers": [
+        "identity"
+      ],
+      "fields": {
+        "description": {
+          "analyzers": [
+            "text_en"
+          ]
+        },
+        "genre": {
+          "analyzers": [
+            "text_en"
+          ]
+        },
+        "label": {
+          "analyzers": [
+            "text_en"
+          ]
+        },
+        "title": {
+          "analyzers": [
+            "text_en"
+          ]
+        },
+        "type": {
+          "analyzers": [
+            "text_en"
+          ]
+        }
+      },
+      "includeAllFields": true,
+      "storeValues": "none",
+      "trackListPositions": false
+    }
+  }
+})
 }
